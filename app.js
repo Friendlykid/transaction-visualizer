6553d55a0f7d5caf825d3f61106c7f80761a23b6
jirka <jirka.kouba@email.cz>
2023-03-03 18:47:24 +0100

    Stránka generuje animace transakcí. Ještě je potřeba přidat odebírání transakcí když jsou načteny do bloku.

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-let bitcoinMempool;
-
-function getBitcoinMempool(){
-    fetch("https://blockchain.info/unconfirmed-transactions?format=json"
-    ).then((response) => response.json()).then(json => bitcoinMempool = json);
-
-}
 
 //Asi nepotřebuju, ale radši zatím nemažu
 //const fileUpload = require('express-fileupload');
@@ -20,17 +13,12 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-setInterval(getBitcoinMempool, 250);
 
 app.get("/", (req, res) =>{
     const file = fs.readFileSync('views/index.html', 'utf-8');
     res.type('html').send(file);
 });
 
-
-app.get("/getBitcoinMempool",(req, res) =>{
-    res.type("json").send(bitcoinMempool);
-})
 
 // Request for getting files. Must be after static get requests!
 app.get("/:fileName", (req, res) =>{
--- views/bitcoin.js
+++ views/bitcoin.js
@@ -2,11 +2,12 @@ let canvas = document.getElementById('canvas');
 const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 let mouse = {
     x: undefined,
     y: undefined
 };
+
+
 
 const colorArray = [
     '#e967fd',
@@ -19,20 +20,20 @@ const colorArray = [
 window.addEventListener('mousemove', ev => {
     mouse.x = ev.x;
     mouse.y=ev.y;
-})
+});
 
 window.addEventListener('resize', () =>{
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     init();
-})
-function Circle(x, y, dx , dy, radius){
+});
+function Circle(x, y, dx , dy, radius, data){
+    this.data = data;
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
-    this.originalradius = radius;
     this.color = colorArray[Math.floor(Math.random()*colorArray.length)];
     this.draw = function (){
         c.beginPath();
@@ -53,45 +54,52 @@ function Circle(x, y, dx , dy, radius){
         this.x+=this.dx;
         this.y+=this.dy;
 
-        //interactivity
-        if((mouse.x - this.x < 50 && mouse.x - this.x > -50)
-            && (mouse.y - this.y < 50 && mouse.y - this.y > -50) && this.radius < 150){
-            this.radius++;
-        }else if(this.radius > this.originalradius && this.radius > 1){
-            this.radius--;
-        }
-
         this.draw();
     }
 
 }
 
-let circle = new Circle(200, 300, 3, -3,30);
+//let circle = new Circle(200, 300, 3, -3,30);
+//circle.update();
 
-let circleArray = [];
+const circles = new Map();
+const bitcoinMempool = new Map();
 
-
+function generateCircle(transaction){
+    let radius = transaction.weight / 100;
+    let x = Math.random()* (window.innerWidth - radius*2) + radius;
+    let y = Math.random()* (window.innerHeight - radius*2) + radius;
+    let dx = 2*(Math.random()-0.5);
+    let dy = 2*(Math.random()-0.5);
+    return new Circle(x,y,dx,dy,radius, transaction);
+}
 function init(){
-    circleArray = [];
-    for(let i = 0; i < 500; i++){
-        let radius = Math.random() * 3 + 2;
-        let x = Math.random()* (window.innerWidth - radius*2) + radius;
-        let y = Math.random()* (window.innerHeight - radius*2) + radius;
-        let dx = 2*(Math.random()-0.5);
-        let dy = 2*(Math.random()-0.5);
-
-        circleArray.push(new Circle(x,y,dx,dy,radius));
-
+    getBitcoinMempool();
+    for (const [, value] of bitcoinMempool){
+        circles.set(value.hash,generateCircle(value));
     }
 }
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0, innerWidth,innerHeight);
-    for(let i = 0;i < circleArray.length;i++){
-        circleArray[i].update();
+    for( const [key, transaction] of bitcoinMempool){
+        if(!circles.has(key))
+            circles.set(key,generateCircle(transaction));
+        circles.get(key).update();
     }
-    circle.update();
+}
+function getBitcoinMempool(){
+    fetch("https://blockchain.info/unconfirmed-transactions?format=json"
+    ).then((response) => response.json()).then((json) => {
+        //if transaction was not in our memory, then it is added
+        for(const tx of json.txs){
+            if(!bitcoinMempool.has(tx.hash))
+                bitcoinMempool.set(tx.hash,tx);
+        }
+    });
+
 }
 
+setInterval(getBitcoinMempool,500);
 init();
 animate();
